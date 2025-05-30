97550034239fac27d32c4878312b6abd931ccc6a
Robert Piosik <robertpiosik@gmail.com>
2025-05-30 22:41:14 +0200

    Integrate configuration and setup for the new Intelligent Update API tool

--- packages/vscode/src/constants/state-keys.ts
+++ packages/vscode/src/constants/state-keys.ts
@@ -10,14 +10,20 @@ export const TOOL_CONFIG_CODE_COMPLETIONS_STATE_KEY =
   'toolConfigCodeCompletions'
 export const TOOL_CONFIG_FILE_REFACTORING_STATE_KEY =
   'toolConfigFileRefactoring'
+export const TOOL_CONFIG_INTELLIGENT_UPDATE_STATE_KEY =
+  'toolConfigIntelligentUpdate'
 export const TOOL_CONFIG_COMMIT_MESSAGES_STATE_KEY = 'toolConfigCommitMessages'
 export const DEFAULT_CODE_COMPLETIONS_CONFIGURATION_STATE_KEY =
   'defaultCodeCompletionsConfiguration'
 export const DEFAULT_FILE_REFACTORING_CONFIGURATION_STATE_KEY =
   'defaultFileRefactoringConfiguration'
+export const DEFAULT_INTELLIGENT_UPDATE_CONFIGURATION_STATE_KEY =
+  'defaultIntelligentUpdateConfiguration'
 export const LAST_SELECTED_CODE_COMPLETION_CONFIG_INDEX_KEY =
   'lastSelectedCodeCompletionConfigIndex'
 export const LAST_SELECTED_FILE_REFACTORING_CONFIG_INDEX_KEY =
   'lastSelectedFileRefactoringConfigIndex'
+export const LAST_SELECTED_INTELLIGENT_UPDATE_CONFIG_INDEX_KEY =
+  'lastSelectedIntelligentUpdateConfigIndex'
 export const LAST_SELECTED_PRESET_KEY = 'lastSelectedPreset'
 export const HOME_VIEW_TYPE_STATE_KEY = 'homeViewType'
