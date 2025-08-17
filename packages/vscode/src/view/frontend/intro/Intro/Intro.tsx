0d3c01c22a638ae129cc909e7c8a65c68958d214
Robert Piosik <robertpiosik@gmail.com>
2025-08-17 04:08:10 +0200

    Update the extension version to 1.261.0 and refine the introductory subtitle

--- packages/vscode/src/view/frontend/intro/Intro/Intro.tsx
+++ packages/vscode/src/view/frontend/intro/Intro/Intro.tsx
@@ -19,7 +19,7 @@ export const Intro: React.FC<Props> = (props) => {
             >
               <div className={styles.top__heading__title}>Code Web Chat</div>
               <div className={styles.top__heading__subtitle}>
-                Free vibe coding
+                The free vibe coding tool
               </div>
             </div>
             <button
