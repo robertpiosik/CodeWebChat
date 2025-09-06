14e5650ebdaee6b310810bfadcdbb903c24b49f6
Robert Piosik <robertpiosik@gmail.com>
2025-09-07 00:56:29 +0200

    Remove the 'qwen3-coder-flash' model from Qwen

--- packages/shared/src/constants/chatbots.ts
+++ packages/shared/src/constants/chatbots.ts
@@ -241,12 +241,12 @@ export const CHATBOTS = {
     },
     default_top_p: 0,
     models: {
+      'qwen3-max-preview': { label: 'Qwen3-Max-Preview' },
       'qwen3-235b-a22b-2507': {
         label: 'Qwen3-235B-A22B-2507'
       },
       'qwen3-coder': { label: 'Qwen3-Coder' },
-      'qwen3-30b-a3b-2507': { label: 'Qwen3-30B-A3B-2507' },
-      'qwen3-coder-flash': { label: 'Qwen3-Coder-Flash' }
+      'qwen3-30b-a3b-2507': { label: 'Qwen3-30B-A3B-2507' }
     }
   },
   Together: {
