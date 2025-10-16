84803ed2042d5cd9fc7d4d03ad1ebc20504c7d7d
Robert Piosik <robertpiosik@gmail.com>
2025-10-16 12:25:25 +0200

    Ensure all workspace files are saved before creating checkpoints

--- packages/vscode/src/commands/checkpoints-command.ts
+++ packages/vscode/src/commands/checkpoints-command.ts
@@ -198,6 +198,7 @@ const create_checkpoint = async (
         cancellable: false
       },
       async () => {
+        await vscode.workspace.saveAll()
         const checkpoint_dir_path = get_checkpoint_path(timestamp)
         const checkpoint_dir_uri = vscode.Uri.file(checkpoint_dir_path)
         await vscode.workspace.fs.createDirectory(checkpoint_dir_uri)
@@ -231,6 +232,7 @@ const create_temporary_checkpoint = async (
       cancellable: false
     },
     async () => {
+      await vscode.workspace.saveAll()
       const timestamp = Date.now()
       const checkpoint_dir_path = get_checkpoint_path(timestamp)
       const checkpoint_dir_uri = vscode.Uri.file(checkpoint_dir_path)
