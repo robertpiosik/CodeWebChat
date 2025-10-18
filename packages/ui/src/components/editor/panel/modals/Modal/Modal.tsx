0d94c03e9538bdd40047ed83a12ceb1be13073ca
Robert Piosik <robertpiosik@gmail.com>
2025-10-18 18:28:21 +0200

    Remove entry animations and hydration logic from the Modal component

--- packages/ui/src/components/editor/panel/modals/Modal/Modal.tsx
+++ packages/ui/src/components/editor/panel/modals/Modal/Modal.tsx
@@ -1,24 +1,12 @@
-import { useEffect, useState } from 'react'
 import styles from './Modal.module.scss'
-import cn from 'classnames'
 
 type Props = {
   children: React.ReactNode
 }
 
 export const Modal: React.FC<Props> = (props) => {
-  const [is_hydrated, set_is_hydrated] = useState(false) // For entry animation
-
-  useEffect(() => {
-    set_is_hydrated(true)
-  }, [])
-
   return (
-    <div
-      className={cn(styles.overlay, {
-        [styles['overlay--visible']]: is_hydrated
-      })}
-    >
+    <div className={styles.overlay}>
       <div className={styles.container}>{props.children}</div>
     </div>
   )
