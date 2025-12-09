53a9e21a77f17a9fb1ab8eb64f1eb819cff9b5a3
Robert Piosik <robertpiosik@gmail.com>
2025-12-09 03:31:25 +0100

    Reduce API request timeout from 10s to 5s to improve responsiveness on failures

--- packages/vscode/src/utils/make-api-request.ts
+++ packages/vscode/src/utils/make-api-request.ts
@@ -261,7 +261,7 @@ export const make_api_request = async (params: {
           },
           cancelToken: params.cancellation_token,
           responseType: 'stream',
-          timeout: 10000
+          timeout: 5000
         }
       )
 
