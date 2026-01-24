64e257bc54fe027da50a71e6de9180b99e4d6cbb
Robert Piosik <robertpiosik@gmail.com>
2026-01-24 02:24:37 +0100

    Update donation message to reference the project instead of independent software development

--- packages/ui/src/components/editor/panel/Donations/Donations.tsx
+++ packages/ui/src/components/editor/panel/Donations/Donations.tsx
@@ -146,8 +146,8 @@ export const Donations: React.FC<DonationsProps> = (props) => {
       <Scrollable>
         <div className={styles.container}>
           <div className={styles.about}>
-            Hey there! Donating is a great way to show your support for
-            independent software development. Thank you.
+            Hey there! Donating is a great way to show your support for the
+            project. Thank you.
           </div>
           <div className={styles.links}>
             <Fieldset
