5585b591bd84538ad164f5a5313d0239f20df877
Robert Piosik <robertpiosik@gmail.com>
2026-01-24 02:16:23 +0100

    Remove privacy emphasis from README and update donation support message

--- packages/ui/src/components/editor/panel/Donations/Donations.tsx
+++ packages/ui/src/components/editor/panel/Donations/Donations.tsx
@@ -147,8 +147,8 @@ export const Donations: React.FC<DonationsProps> = (props) => {
       <Scrollable>
         <div className={styles.container}>
           <div className={styles.about}>
-            Hey there! Donating is a great way to show your support for the
-            project. Thank you.
+            Hey there! Donating is a great way to show your support for
+            independent software development. Thank you.
           </div>
           <div className={styles.sponsors}>
             <Fieldset
