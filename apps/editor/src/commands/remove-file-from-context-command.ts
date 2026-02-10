2a42a0f22385f744808e7a7c3c41c86eab1a3a67
Robert Piosik <robertpiosik@gmail.com>
2026-02-10 03:04:27 +0100

    Remove custom close button and simplify quick pick navigation logic

--- apps/editor/src/commands/remove-file-from-context-command.ts
+++ apps/editor/src/commands/remove-file-from-context-command.ts
@@ -158,22 +158,12 @@ export const remove_file_from_context_command = (
             label: f.label,
             full_path: f.full_path
           }))
-          folder_quick_pick.buttons = [
-            vscode.QuickInputButtons.Back,
-            {
-              iconPath: new vscode.ThemeIcon('close'),
-              tooltip: 'Close'
-            }
-          ]
+          folder_quick_pick.buttons = [vscode.QuickInputButtons.Back]
 
           let folder_accepted = false
-          let should_close_fully = false
 
           folder_quick_pick.onDidTriggerButton((button) => {
             if (button === vscode.QuickInputButtons.Back) {
-              folder_quick_pick.hide()
-            } else if (button.tooltip == 'Close') {
-              should_close_fully = true
               folder_quick_pick.hide()
             }
           })
@@ -209,11 +199,6 @@ export const remove_file_from_context_command = (
           folder_quick_pick.onDidHide(() => {
             folder_quick_pick.dispose()
             is_showing_folder_quick_pick = false
-
-            if (should_close_fully) {
-              quick_pick.dispose()
-              return
-            }
 
             if (!folder_accepted) {
               if (file_items_cache.length > 0) {
