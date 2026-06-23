e344df882451b207f0d77870308d023cc72b6428
Robert Piosik <robertpiosik@gmail.com>
2026-06-24 01:07:48 +0200

    Remove default model assignment from new web configuration
    - newly created configuration has model assigned (first on the list), should not.

--- apps/editor/src/views/actions/create-web-configuration.ts
+++ apps/editor/src/views/actions/create-web-configuration.ts
@@ -15,14 +15,72 @@ export const create_web_configuration = async (params: {
   let insertion_index: number | undefined
 
   if (params.reference_index !== undefined) {
-    const position_quick_pick = await new Promise<string | undefined>((resolve) => {
+    const position_quick_pick = await new Promise<string | undefined>(
+      (resolve) => {
+        const quick_pick = vscode.window.createQuickPick()
+        quick_pick.items = [
+          { label: 'Insert a new item above' },
+          { label: 'Insert a new item below' }
+        ]
+        quick_pick.title = 'Placement'
+        quick_pick.placeholder = 'Where to insert?'
+        quick_pick.buttons = [
+          {
+            iconPath: new vscode.ThemeIcon('close'),
+            tooltip: 'Close'
+          }
+        ]
+
+        let accepted = false
+        const disposables: vscode.Disposable[] = []
+
+        disposables.push(
+          quick_pick.onDidTriggerButton(() => {
+            quick_pick.hide()
+          }),
+          quick_pick.onDidAccept(() => {
+            accepted = true
+            resolve(quick_pick.selectedItems[0]?.label)
+            quick_pick.hide()
+          }),
+          quick_pick.onDidHide(() => {
+            if (!accepted) resolve(undefined)
+            disposables.forEach((d) => d.dispose())
+            quick_pick.dispose()
+          })
+        )
+
+        quick_pick.show()
+      }
+    )
+
+    if (!position_quick_pick) return undefined
+
+    insertion_index =
+      position_quick_pick == 'Insert a new item above'
+        ? params.reference_index
+        : params.reference_index + 1
+  } else if (params.placement == 'top') {
+    insertion_index = 0
+  }
+
+  const selected_chatbot = await new Promise<keyof typeof CHATBOTS | undefined>(
+    (resolve) => {
+      const chatbots = Object.entries(CHATBOTS)
+      const items: vscode.QuickPickItem[] = chatbots.map(
+        ([chatbot, { url }]) => ({
+          label: chatbot,
+          description:
+            chatbot == 'Open WebUI'
+              ? 'localhost'
+              : url.replace(/^https?:\/\//, '').split('/')[0]
+        })
+      )
+
       const quick_pick = vscode.window.createQuickPick()
-      quick_pick.items = [
-        { label: 'Insert a new item above' },
-        { label: 'Insert a new item below' }
-      ]
-      quick_pick.title = 'Placement'
-      quick_pick.placeholder = 'Where to insert?'
+      quick_pick.items = items
+      quick_pick.title = 'Chatbots'
+      quick_pick.placeholder = 'Choose a chatbot for the new web configuration'
       quick_pick.buttons = [
         {
           iconPath: new vscode.ThemeIcon('close'),
@@ -39,8 +97,10 @@ export const create_web_configuration = async (params: {
         }),
         quick_pick.onDidAccept(() => {
           accepted = true
-          resolve(quick_pick.selectedItems[0]?.label)
+          const chatbot = quick_pick.selectedItems[0]
+            ?.label as keyof typeof CHATBOTS
           quick_pick.hide()
+          resolve(chatbot)
         }),
         quick_pick.onDidHide(() => {
           if (!accepted) resolve(undefined)
@@ -50,71 +110,19 @@ export const create_web_configuration = async (params: {
       )
 
       quick_pick.show()
-    })
-
-    if (!position_quick_pick) return undefined
-
-    insertion_index =
-      position_quick_pick == 'Insert a new item above'
-        ? params.reference_index
-        : params.reference_index + 1
-  } else if (params.placement == 'top') {
-    insertion_index = 0
-  }
-
-  const selected_chatbot = await new Promise<keyof typeof CHATBOTS | undefined>((resolve) => {
-    const chatbots = Object.entries(CHATBOTS)
-    const items: vscode.QuickPickItem[] = chatbots.map(([chatbot, { url }]) => ({
-      label: chatbot,
-      description:
-        chatbot == 'Open WebUI' ? 'localhost' : url.replace(/^https?:\/\//, '').split('/')[0]
-    }))
-
-    const quick_pick = vscode.window.createQuickPick()
-    quick_pick.items = items
-    quick_pick.title = 'Chatbots'
-    quick_pick.placeholder = 'Choose a chatbot for the new web configuration'
-    quick_pick.buttons = [
-      {
-        iconPath: new vscode.ThemeIcon('close'),
-        tooltip: 'Close'
-      }
-    ]
-
-    let accepted = false
-    const disposables: vscode.Disposable[] = []
-
-    disposables.push(
-      quick_pick.onDidTriggerButton(() => {
-        quick_pick.hide()
-      }),
-      quick_pick.onDidAccept(() => {
-        accepted = true
-        const chatbot = quick_pick.selectedItems[0]?.label as keyof typeof CHATBOTS
-        quick_pick.hide()
-        resolve(chatbot)
-      }),
-      quick_pick.onDidHide(() => {
-        if (!accepted) resolve(undefined)
-        disposables.forEach((d) => d.dispose())
-        quick_pick.dispose()
-      })
-    )
-
-    quick_pick.show()
-  })
+    }
+  )
 
   if (!selected_chatbot) return undefined
 
   const new_name = generate_unique_name(
     undefined,
-    current_web_configurations.map(c => c.name)
+    current_web_configurations.map((c) => c.name)
   )
 
   const new_web_configuration: ConfigWebConfigurationFormat = {
     name: new_name,
     chatbot: selected_chatbot,
-    model: Object.keys(CHATBOTS[selected_chatbot].models ?? {})[0],
     systemInstructions: CHATBOTS[selected_chatbot].supports_system_instructions
       ? CHATBOTS[selected_chatbot].default_system_instructions
       : undefined
