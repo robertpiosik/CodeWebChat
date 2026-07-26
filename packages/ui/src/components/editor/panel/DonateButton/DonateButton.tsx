6f91ad0d6a0a6a7a20f63d27e729b777b93d8adc
Robert Piosik <robertpiosik@gmail.com>
2026-07-26 02:05:26 +0200

    Refactor donate button icon container to use a div and BEM styling

--- packages/ui/src/components/editor/panel/DonateButton/DonateButton.tsx
+++ packages/ui/src/components/editor/panel/DonateButton/DonateButton.tsx
@@ -9,9 +9,9 @@ export type DonateButtonProps = {
 export const DonateButton: React.FC<DonateButtonProps> = (props) => {
   return (
     <button className={styles.button} onClick={props.on_click}>
-      <span className={styles['icon-wrapper']}>
+      <div className={styles.button__icon}>
         <span className={cn('codicon', 'codicon-coffee')} />
-      </span>
+      </div>
       <span>{props.label}</span>
     </button>
   )
