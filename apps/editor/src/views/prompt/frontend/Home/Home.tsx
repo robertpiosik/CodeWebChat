d15fe7cbad0c95d65c1bff9b42612470b8913b81
Robert Piosik <robertpiosik@gmail.com>
2026-09-14 03:11:19 +0200

    Add home icon to header and refine target button and tab styles
    - restore the header [Commit]
    - in not hovered state, target button should have gradient: left side - nominal hover color to transparent
    - the color of left-side web/api text should be always in its hover color, so it stays the same, no matter if hovered or not
    - compact target should not have gradient, but the default state from before changes [Changes]
    
    ├── apps
    │   └── editor
    │       └── src
    │           └── views
    │               └── prompt
    │                   └── frontend
    │                       └── Home
    │                           ├── Home.module.scss
    │                           └── Home.tsx
    └── packages
        └── ui
            └── src
                └── components
                    └── editor
                        ├── common
                        │   └── Icon
                        │       └── Icon.tsx
                        └── prompt
                            ├── Tabs
                            │   └── Tabs.module.scss
                            └── TargetButton
                                ├── TargetButton.module.scss
                                ├── TargetButton.stories.tsx
                                ├── TargetButton.tsx
                                └── index.ts

--- apps/editor/src/views/prompt/frontend/Home/Home.tsx
+++ apps/editor/src/views/prompt/frontend/Home/Home.tsx
@@ -142,18 +142,23 @@ export const Home: React.FC<Props> = (props) => {
           })}
           ref={container_ref}
         >
-          <CompactableActionButton
-            label={discord_label}
-            href="https://discord.gg/KJySXsrSX5"
-            icon="DISCORD"
-            is_compact={compact_step >= 1}
-          />
-          <CompactableActionButton
-            label={coffee_label}
-            on_click={props.on_donate_click}
-            codicon="coffee"
-            is_compact={compact_step >= 2}
-          />
+          <div className={styles['header__left']}>
+            <span className="codicon codicon-home" />
+          </div>
+          <div className={styles['header__right']}>
+            <CompactableActionButton
+              label={discord_label}
+              href="https://discord.gg/KJySXsrSX5"
+              icon="DISCORD"
+              is_compact={compact_step >= 1}
+            />
+            <CompactableActionButton
+              label={coffee_label}
+              on_click={props.on_donate_click}
+              codicon="coffee"
+              is_compact={compact_step >= 2}
+            />
+          </div>
         </div>
 
         <div
