803393ee25ccba7ed8dac1bda26329469cfc0c1a
Robert Piosik <robertpiosik@gmail.com>
2026-09-20 22:00:19 +0200

    Trim trailing character from continue in browser modal title
    - using js, strip last char in [Fragment] (dot)
    
    └── apps
        └── editor
            └── src
                ├── i18n
                │   └── translations
                │       └── common.ts
                └── views
                    └── prompt
                        └── backend
                            └── message-handlers
                                └── handle-autofill.ts

--- apps/editor/src/views/prompt/backend/message-handlers/handle-autofill.ts
+++ apps/editor/src/views/prompt/backend/message-handlers/handle-autofill.ts
@@ -116,7 +116,7 @@ export const handle_autofill = async (params: {
   if (sent) {
     params.prompt_view_provider.send_message({
       command: 'SHOW_AUTO_CLOSING_MODAL',
-      title: t('common.info.continue-in-browser')
+      title: t('common.info.continue-in-browser').slice(0, -1)
     })
   }
 
