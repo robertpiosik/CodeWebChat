f6ec58625fe09db929846783c76ea5ef8d090b72
Robert Piosik <robertpiosik@gmail.com>
2026-09-20 22:51:12 +0200

    Increase spacer height in landscape configurations view

--- apps/editor/src/views/prompt/frontend/Main/MainView/MainView.tsx
+++ apps/editor/src/views/prompt/frontend/Main/MainView/MainView.tsx
@@ -590,7 +590,7 @@ export const MainView: React.FC<Props> = (props) => {
         >
           <UiScrollable scroll_to_top_key={props.scroll_reset_key} top_shadow>
             <div className={styles.content}>
-              <UiSpacer height={4} />
+              <UiSpacer height={6} />
               {configurations_section}
               {props.bottom_spacer_height !== undefined &&
                 props.bottom_spacer_height > 0 && (
